f0d20f4c6333c173209adf35c844ba20bbd31ffd
elicep01 <epriyadarshi@wisc.edu>
2025-12-11 19:17:34 -0600

    Remove unrelated DeepWater pipeline, notebooks, and old scripts

--- deepwater/deepwater/__init__.py
+++
@@ -1,2 +0,0 @@
-"""DeepWater - Water Level Estimation from Imagery Using Computer Vision."""
-__version__ = "0.1.0"
--- deepwater/deepwater/training/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Training module for DeepWater."""
-from .trainer import (
-    WaterLevelTrainer, TrainingMetrics, EarlyStopping,
-    WarmupScheduler, create_optimizer, create_scheduler,
-)
-__all__ = ["WaterLevelTrainer", "TrainingMetrics", "EarlyStopping",
-           "WarmupScheduler", "create_optimizer", "create_scheduler"]
